cc74cad97e976cf2a784d700bbc87b02422667ce
Stypian <157755550+Stypian@users.noreply.github.com>
2024-05-15 08:15:18 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,23 +2,26 @@ onload = function() {
 
   const markAsRead = () => {
     const readNot = document.querySelector(".notifications__p--read");
-    const newNot = document.querySelectorAll(".notifications__new");
     const notStatus = document.querySelectorAll(".notifications__status");
     const notNumber = document.querySelector(".notifications__number");
 
     //remove the unread dot from messages
     function remStatus() {
-        for (let i = 0; i < notStatus.length; i++) {
-            notStatus[i].classList.remove("notifications__unread");
+        for (let i = 0; i < notWrap.length; i++) {
+            if (notStatus[i].classList.contains("notifications__unread")) {
+                notStatus[i].classList.remove("notifications__unread");
+            }
         }
     }
 
     //change background color of unread and change new notifications number to zero
-    function markRead() {
-        for (let i = 0; i < newNot.length; i++) {
-            newNot[i].classList.remove("notifications__new");
+     function markRead() {
+       for (let i = 0; i < notWrap.length; i++) {
+        if (notWrap[i].classList.contains("notifications__new")) {
+            notWrap[i].classList.remove("notifications__new");
             notNumber.innerText = '0';
         }
+       }
     }
     
     readNot.addEventListener("click", function() {
